4a845a1840b01a9c2a82116706485eebd39360c4
Xannetz Suficiencia <xvsuficiencia@gmail.com>
2023-05-16 09:23:47 +0800

    Added Buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styleSheet/componentCSS.css";
 import Body from "./components/transparentDiv";
 import mountain from './images/mountain.jpg';
+import SampleButton from "./components/buttons";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Body />
         <div class="gradient fitScreen layering1" />
+        <SampleButton />
         <img src={mountain} class="fitScreen layering2"/>
       </div>
     </>
---
+++ src/components/buttons.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../styleSheet/componentCSS.css";
+
+function SampleButton() {
+    return (
+        <>
+            <button class="button1">sample button 1</button>
+            <button class="button2">sample button 2</button>
+            <button class="button3">sample button 3</button>
+            <button class="button4">sample button 4</button>
+        </>
+    );
+}
+
+export default SampleButton;
